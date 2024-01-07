cb040abb57692ad5a4b8492b25cb80f9e0d4dfba
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-07 20:54:43 +0100

    T022_Loop

---
+++ T022_Loop/Program.cs
@@ -0,0 +1,44 @@
+﻿int[] intsToCompress = [10, 15, 20, 25, 30, 12, 34];
+
+Console.WriteLine(intsToCompress.Sum()); // 146
+
+int totalValue = 0;
+
+for (int i = 0; i < intsToCompress.Length; i++)
+{
+	totalValue += intsToCompress[i];
+}
+Console.WriteLine(totalValue);
+
+totalValue = 0;
+foreach (var intForCompression in intsToCompress)
+{
+	totalValue += intForCompression;
+}
+Console.WriteLine(totalValue);
+
+totalValue = 0;
+int index = 0;
+while (index < intsToCompress.Length)
+{
+	totalValue += intsToCompress[index++];
+}
+Console.WriteLine(totalValue);
+
+totalValue = 0;
+index = 0;
+do
+{
+	totalValue += intsToCompress[index++];
+} while (index < intsToCompress.Length);
+Console.WriteLine(totalValue);
+
+totalValue = 0;
+foreach (var intForCompression in intsToCompress)
+{
+	if (intForCompression > 20)
+	{
+		totalValue += intForCompression;
+	}
+}
+Console.WriteLine(totalValue);
