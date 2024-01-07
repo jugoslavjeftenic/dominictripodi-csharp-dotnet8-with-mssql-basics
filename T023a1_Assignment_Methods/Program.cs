bc4bf668252f63012c8ff4ad41d36ecbdbbdd3f9
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-07 21:54:31 +0100

    T023a1_Assignment_Methods

---
+++ T023a1_Assignment_Methods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		List<int> myNumberList = [2, 3, 5, 6, 7, 9, 10, 123, 324, 54];
+
+		foreach (int number in myNumberList)
+		{
+			PrintIfOdd(number);
+		}
+	}
+
+	private static void PrintIfOdd(int number)
+	{
+		if (number % 2 > 0)
+		{
+			Console.WriteLine(number);
+		}
+	}
+}
