63b744bf14a71bf2cd3614191c84157a923e63af
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-07 16:12:23 +0100

    T019a1_Assignment_OperatorsAndConditionals

---
+++ T019a1_Assignment_OperatorsAndConditionals/Program.cs
@@ -0,0 +1,7 @@
+﻿int myFirstValue = 7;
+int mySecondValue = 5;
+
+Console.WriteLine(myFirstValue + mySecondValue);
+Console.WriteLine(myFirstValue - mySecondValue);
+Console.WriteLine(myFirstValue * mySecondValue);
+Console.WriteLine(myFirstValue > mySecondValue);
