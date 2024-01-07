6fd279d8c19be522424a6ec76c0f16b2c17a9d25
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-07 17:15:23 +0100

    T021_ConditionalStatements

---
+++ T021_ConditionalStatements/Program.cs
@@ -0,0 +1,41 @@
+﻿int myInt = 5;
+int mySecondInt = 10;
+
+if (myInt > mySecondInt)
+{
+	myInt += 10;
+}
+
+Console.WriteLine(myInt);
+
+string myCow = "cow";
+string myCapitalizedCow = "Cow";
+
+if (myCow == myCapitalizedCow)
+{
+	Console.WriteLine("Equal");
+}
+else if (myCow == myCapitalizedCow.ToLower())
+{
+	Console.WriteLine("Equal without case sensitivity.");
+}
+else
+{
+	Console.WriteLine("Not Equal");
+}
+
+switch (myCow)
+{
+	case "cow":
+		Console.WriteLine("Lowercase");
+		break;
+	case "Cow":
+		Console.WriteLine("Capitalized");
+		break;
+	case "Cows":
+		Console.WriteLine("Multiple cows");
+		break;
+	default:
+		Console.WriteLine("Default");
+		break;
+}
