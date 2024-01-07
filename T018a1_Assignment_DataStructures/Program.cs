2e62d64af9058142e373cad3660e1dd480a86e37
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-07 15:27:14 +0100

    T018a1_Assignment_DataStructures

---
+++ T018a1_Assignment_DataStructures/Program.cs
@@ -0,0 +1,9 @@
+﻿Dictionary<string, decimal> itemPrices = new()
+{
+	{"cheese",5.99m },
+	{"carrots",2.99m }
+};
+
+Console.WriteLine(itemPrices["cheese"].GetType());
+Console.WriteLine(itemPrices["cheese"]);
+Console.WriteLine(itemPrices["carrots"]);
