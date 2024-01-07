d079d13cf5fbd5c5da2614df20638719a1c3cf2f
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-07 17:24:57 +0100

    T021a1_Assignment_ConditionalStatements

---
+++ T021a1_Assignment_ConditionalStatements/Program.cs
@@ -0,0 +1,15 @@
+﻿string myFirstValue = "some words";
+string mySecondValue = "Some Words";
+
+if (myFirstValue.Equals(mySecondValue))
+{
+	Console.WriteLine("equal");
+}
+if (myFirstValue.Equals(mySecondValue.ToLower()))
+{
+	Console.WriteLine("equal without case sensitivity");
+}
+else
+{
+	Console.WriteLine("not equal");
+}
