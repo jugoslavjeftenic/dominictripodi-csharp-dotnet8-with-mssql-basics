f4abf43af03a8b3f9f2448aeb7a3745c017f1666
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-07 22:14:34 +0100

    T024_Scope

---
+++ T024_Scope/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+	static int _accessibleInt = 7;
+
+	private static void Main(string[] args)
+	{
+		Console.WriteLine(_accessibleInt);
+
+		TestMethod();
+	}
+
+	static void TestMethod()
+	{
+		int accessibleInt = 5;
+		Console.WriteLine(accessibleInt);
+	}
+}
