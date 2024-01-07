352812d2614a5cf90b97314e1ff9ed7886dd9d48
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-07 21:49:03 +0100

    T023_Methods

---
+++ T023_Methods/Program.cs
@@ -0,0 +1,22 @@
+﻿
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		int[] intsToCompress = [10, 15, 20, 25, 30, 12, 34];
+
+		_ = GetSum(intsToCompress);
+	}
+
+	private static int GetSum(int[] intsToCompress)
+	{
+		int totalValue = 0;
+
+		foreach (var n in intsToCompress)
+		{
+			totalValue += n;
+		}
+
+		return totalValue;
+	}
+}
