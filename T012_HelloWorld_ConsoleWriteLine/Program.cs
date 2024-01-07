c452da78e5660a892f86912062b14c65ef95f79c
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-07 01:53:43 +0100

    T012_HelloWorld_ConsoleWriteLine

---
+++ T012_HelloWorld_ConsoleWriteLine/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello C# Programmers.");
+Console.Write("First");
+Console.Write("Second");
+Console.WriteLine("Third");
