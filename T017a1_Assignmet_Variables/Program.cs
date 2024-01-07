cb91ae5dc07fcd85bee8a0ca01f56077c5170042
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-07 03:16:50 +0100

    T017a1_Assignmet_Variables

---
+++ T017a1_Assignmet_Variables/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+string myString = "Some string";
+decimal myDecimal = 1.1m;
+bool myBoolean = true;
+
+Console.WriteLine(myString.GetType());
+Console.WriteLine(myDecimal.GetType());
+Console.WriteLine(myBoolean.GetType());
